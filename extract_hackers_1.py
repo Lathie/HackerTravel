00d86071dde4f6256d7741e6dded886c3ccdf3d0
Lathie <niuvictor@gmail.com>
2017-01-12 00:00:48 -0600

    finished v1

--- extract_hackers_1.py
+++ extract_hackers_1.py
@@ -1,13 +1,8 @@
 
 import csv
 import sys
+import pprint
 
-school_dict = {} #counts hackers accepted from schools
-total_accepted = 0
-school_dict_total = {} #counts hackers applied from schools
-total_applied = 0
-school_bus_requests = {}
-bus_requests = 0
 
 def extract():
     try:
@@ -16,42 +11,78 @@ def extract():
 
         csv_input.next() #Skip column names
 
+        global school_dict
+        global school_dict_total
+        global school_bus_requests
+
+        school_dict = {} #counts hackers accepted from schools
+        school_dict_total = {} #counts hackers applied from schools
+        school_bus_requests = {}
+
+        global total_accepted
+        global bus_requests
+        global total_applied
+
+        total_accepted = 0
+        bus_requests = 0
+        total_applied = 0
         for row in csv_input:
-            #print str(row[15])
-            #START COUNTING THINGS NOW :DDD
-            if row[15].upper() not in school_dict:
-                school_dict[row[15].upper()] = 0
+
+            cur_school = row[15].upper()
+            cur_school = cur_school.replace(',', '')
+
+            #START COUNTING THINGS NOW :DD
+
+            if cur_school not in school_dict:
+                school_dict[cur_school] = 0
             if row[4] == "ACCEPTED":
-                school_dict[row[15].upper()] += 1
+                school_dict[cur_school] += 1
                 total_accepted += 1
 
-            if row[15].upper() not in school_bus_requests:
-                school_bus_requests[row[15].upper()] = 0
+            if cur_school not in school_bus_requests:
+                school_bus_requests[cur_school] = 0
             if row[4] == "ACCEPTED":
                 if "BUS" in row[9]:
-                    print row[9]
-                    school_bus_requests[row[15].upper()] += 1
+                    #print row[9]
+                    school_bus_requests[cur_school] += 1
                     bus_requests += 1
 
-            if row[15].upper() not in school_dict_total:
-                school_dict_total[row[15].upper()] = 0
-            school_dict_total[row[15].upper()] += 1
+            if cur_school not in school_dict_total:
+                school_dict_total[cur_school] = 0
+            school_dict_total[cur_school] += 1
             total_applied += 1
 
             #THATS ALL FOLKS
         csv_raw.close()
 
-        for thing in school_dict:
-            print thing + "," + str(school_dict[thing]) + "," + str(school_dict_total[thing]) + "," + str(school_bus_requests[row[15].upper()])
+        #for thing in school_dict:
+        #    print thing + "," + str(school_dict[thing]) + "," + str(school_dict_total[thing]) + "," + str(school_bus_requests[thing])
 
-        print "accepted: " + str(total_accepted)
-        print "applied: " + str(total_applied)
-        print "bus requests: " + str(bus_requests)
+        #print "accepted: " + str(total_accepted)
+        #print "applied: " + str(total_applied)
+        #print "bus requests: " + str(bus_requests)
 
-
+        #pprint.pprint(school_bus_requests, width=1)
 
     except IOError as error:
         print "NO CSV FILE"
         sys.exit(1)
 
-extract()
+
+def model():
+    global results
+    results = {}
+
+    for thing in school_dict:
+        multi = float(school_dict[thing])/float(total_accepted)
+        results[thing] = 100 * float(school_bus_requests[thing]) * multi
+
+    for thing in results:
+        if results[thing] == 0:
+            continue
+        print thing + "," + str(results[thing])
+
+
+if __name__ == "__main__":
+    extract()
+    model()
